afc0397d95ee0c5722317bfb5e98e29441ba42bb
Nama Kamu <emailkamu@example.com>
2025-12-13 22:05:54 +0700

    add : force-dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import HeroSection from "./components/HeroSection";
 
 export default function HomePage() {
--- src/app/undanganuntuk/page.js
+++ src/app/undanganuntuk/page.js
@@ -1,5 +1,5 @@
 import HeroSection from "../components/HeroSection";
 
-export default function HomePage() {
+export default function Undangan() {
   return <HeroSection />;
 }

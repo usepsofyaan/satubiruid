9e936ab2ee060fc90cbdb99fde22bdba3517acb8
Nama Kamu <emailkamu@example.com>
2025-12-13 22:17:38 +0700

    add : suspense 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import HeroSection from "./components/HeroSection";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
-  return <HeroSection />;
+  return (
+    <Suspense fallback={<div>Memuat undangan...</div>}>
+      <HeroSection />
+    </Suspense>
+  );
 }
